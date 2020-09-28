f57127439cebdf5297d399664b0357c6a58b82fc
ybgirgin3 <ybgirgin3@gmail.com>
2020-09-28 19:46:53 +0300

    ads

--- python_/triangle_guest.py
+++ python_/triangle_guest.py
@@ -1,4 +1,6 @@
 # complete in one print statement 
 for i in range(1,int(input())+1):
         #print(i)
-        print(int(((10**i-1)/(9))**2))
+        #print(int(((10**i-1)/(9))**2))
+        #print(f'{i}'*i)
+        print(int(((10 ** i - 1) / 9) * i))
---
+++ python_/triangle_guest2.py
@@ -0,0 +1,4 @@
+# complete in one print statement 
+for i in range(1,int(input())+1):
+        #print(i)
+        print(int(((10**i-1)/(9))**2))
