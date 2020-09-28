87a63d9eda537f33b3892bb17773de391b7ad96d
ybgirgin3 <ybgirgin3@gmail.com>
2020-09-28 21:35:23 +0300

    asd

---
+++ python_/mean_std.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+n, m = input().split()
+arr = np.array([input().split() for _ in range(int(n))], int)
+
+print(np.mean(arr, axis=1))
+print(np.var(arr, axis=0))
+x = np.std(arr)
+print(round(x, 11)
+
