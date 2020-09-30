022b8d15ac0bb7c2e108d6a5eef680033e92331c
ybgirgin3 <ybgirgin3@gmail.com>
2020-09-30 20:38:16 +0300

    asd

---
+++ python_/maximize_it.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+x = input().split()
+l = []
+for _ in range(int(x[0])):
+    l.append(
+
+
+"""
+x = input().split()
+nums = [input().split() for _ in range(int(x[0]))]
+#nums = [i for i in [input().split() for _ in range(int(x[0]))]]
+for num in nums:
+    print(max(num))
+
+#all_ = [i for i in nums]
+
+print()
+for lists_ in all_:
+    #print(max(lists_))
+    ret = max(lists_)
+    print(ret)
+
+
+"""
