a1c8a13ae47809410d0be2d904d5c5dfdec819f1
ybgirgin3 <ybgirgin3@gmail.com>
2020-10-02 02:58:53 +0300

    asd

--- python_/mean_std.py
+++ python_/mean_std.py
@@ -1,11 +1,9 @@
 import numpy as np
+np.set_printoptions(sign=' ')
 
 n, m = input().split()
 arr = np.array([input().split() for _ in range(int(n))], int)
 
 print(np.mean(arr, axis=1))
 print(np.var(arr, axis=0))
-x = np.std(arr)
-print(round(x, 11))
-
-
+print(np.around(np.std(arr), decimals=12))
