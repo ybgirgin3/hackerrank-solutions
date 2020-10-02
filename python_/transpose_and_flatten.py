c4d3805b6b5ef10b330142fa9993fe8ce0ad0610
ybgirgin3 <ybgirgin3@gmail.com>
2020-10-02 03:15:35 +0300

    asd

--- python_/transpose_and_flatten.py
+++ python_/transpose_and_flatten.py
@@ -1,7 +1,7 @@
 import numpy as np
 n,m = map(int, input().split())
 
-arr = np.array( [list(map(int, input().split())) for _ in range(m)] )
+arr = np.array([input().split() for _ in range(n)], int)
 
 print(arr.T)
 #print(arr.flatten)
