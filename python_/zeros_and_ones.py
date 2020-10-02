d151fbfd44732815d34ae157343180737b7cf286
ybgirgin3 <ybgirgin3@gmail.com>
2020-10-02 14:18:34 +0300

    asd

--- python_/zeros_and_ones.py
+++ python_/zeros_and_ones.py
@@ -1,13 +1,4 @@
-import numpy as np
-# 3 3 3 girilecek 3x3 olan 3 tane mtx
-
-x = list(map(int, input().split()))
-
-for _ in range(x[2]):
-    print()
-    print(np.zeros((x[0], x[1])))
-
-for _ in range(x[2]):
-    print(np.ones((x[0], x[1])))
-    print()
-
+import numpy
+N = tuple(map(int, input().split()))
+print(numpy.zeros(N, int))
+print(numpy.ones(N, int))
