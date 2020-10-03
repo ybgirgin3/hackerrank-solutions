56484768faae564c8e525a8bc525c5f1fa269c8b
ybgirgin3 <ybgirgin3@gmail.com>
2020-10-03 19:40:49 +0300

    asd

--- python_/input_.py
+++ python_/input_.py
@@ -1,11 +1,2 @@
-asd = input().split()
-x = int(asd[0])
-k = int(asd[1])
-
-pol = int(input())
-
-for i in pol:
-    if i == 'x':
-        i == x
-
-print(pol)
+x, k = map(int, input().split())
+print(input() == k)
