887ae22bcb24febab3cf2f5fb4953542f8560f58
ybgirgin3 <ybgirgin3@gmail.com>
2020-10-10 22:59:52 +0300

    cpp added

---
+++ C++/basic_data_types.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+
+
+int main() {
+  // variables
+  int a;
+  long b;
+  char c;
+  float d;
+  double e;
+
+  // getting inputs
+  cin >> a >> b >> c >> d >> e;
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+  cout << e << endl;
+
+
+  return 0;
+}
