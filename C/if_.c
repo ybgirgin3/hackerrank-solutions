58c4f62585d115eff3e1a43595d6a8375f185696
ybgirgin3 <ybgirgin3@gmail.com>
2021-06-13 21:12:22 +0300

    C added

---
+++ C/if_.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char inToNum();
+
+
+
+int main()
+{
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char inToNum(int num) {
+  if (num == 1) {
+    return "one";
+  }
+  else if (num == 2) {
+      return "two";
+    }
+  else if (num == 3) {
+    return "three";
+    }
+  else if (num == 4) {
+    return "four";
+  }
+  else if (num == 5) {
+    return "five";
+    }
+  else if (num == 6) {
+    return "six";
+  }
+  else if (num == 7) {
+    return "seven";
+  }
+  else if (num == 8) {
+    return "eight";
+  }
+  else if (num == 9) {
+    return "nine"
+  }
+  else if (num > 9) {
+    return "Greater than 9";
+  }
+
+}
